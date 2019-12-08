193dbdb4621f04be2645fc86a2d5bf31511844f3
wzh <yxwangzhenhua@163.com>
2019-12-08 10:01:17 +0800

    empdao层接口开发

---
+++ src/main/java/com/wzh/ems/entity/dao/EmpDao.java
@@ -0,0 +1,10 @@
+package com.wzh.ems.entity.dao;
+
+import com.wzh.ems.entity.Emp;
+import tk.mybatis.mapper.additional.idlist.DeleteByIdListMapper;
+import tk.mybatis.mapper.additional.idlist.IdListMapper;
+import tk.mybatis.mapper.additional.insert.InsertListMapper;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface EmpDao extends Mapper<Emp>, DeleteByIdListMapper<Emp,String>, IdListMapper<Emp,String>, InsertListMapper<Emp> {
+}
