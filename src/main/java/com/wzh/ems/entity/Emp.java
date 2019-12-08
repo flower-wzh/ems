12d417e7d43a812aa6350fdb579788e0b95769ec
wzh <yxwangzhenhua@163.com>
2019-12-08 11:08:29 +0800

    实体类通用mapper注解

--- src/main/java/com/wzh/ems/entity/Emp.java
+++ src/main/java/com/wzh/ems/entity/Emp.java
@@ -1,20 +1,27 @@
 package com.wzh.ems.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Emp {
+    @Id
+    @Column(name = "empId")
     private String empId;
+    @Column(name = "empName")
     private String empName;
     private Double salary;
     private Integer age;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
 }
