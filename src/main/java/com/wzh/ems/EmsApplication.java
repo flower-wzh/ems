fb8d2d91cbab68ee037f2cc8d6724c80fdae38c6
wzh <yxwangzhenhua@163.com>
2019-12-08 11:11:22 +0800

    入口类注解开发

--- src/main/java/com/wzh/ems/EmsApplication.java
+++ src/main/java/com/wzh/ems/EmsApplication.java
@@ -2,8 +2,10 @@ package com.wzh.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.wzh.ems.dao")
 public class EmsApplication {
 
 	public static void main(String[] args) {
